2cb049fdecb8a72c0f11eb5f9d70b2126ddab612
Kevin <alu0100772999@ull.edu.es>
2018-10-10 16:58:00 +0100

    añadido otra clase al class = item

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+
 var numero = document.getElementById("numero");
 var boton = document.getElementById("boton");
 
 boton.onclick = () => numero.textContent = Math.floor(Math.random()*5+1);
-
 
 var jugador = document.getElementById("jugador");
 var cpu = document.getElementById("CPU");
@@ -13,12 +13,16 @@ function createNumbers(player){
     var randomNumbers = _.shuffle(_.range(1,90));
 
     for (var i of _.range(0,14)){
-        var item = document.createElement("div");
-        item.className = "item";
+        var item = document.createElement("item");
+        item.className = `item ${i}`;
         item.textContent = randomNumbers[i];
+        randomNumbers.splice(i,1);
         player.appendChild(item);
     }
 }
 
 createNumbers(jugador);
 createNumbers(cpu);
+
+function stricke(){
+}
